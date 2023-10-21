2b1e0d59072193b16dd920f82333ee558af89881
Hossain-sakib <mskib567@gmail.com>
2023-10-21 16:26:17 +0600

    fixed the delete issue and completed the project

--- index.js
+++ index.js
@@ -77,6 +77,11 @@ async function run() {
       res.send(result);
     });
 
+
+
+
+
+
     // cart
     app.post("/cart", async (req, res) => {
       const item = req.body;
@@ -90,9 +95,10 @@ async function run() {
       res.send(result);
     });
 
+
     app.delete("/cart/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)};
+      const query = { _id: id};
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
